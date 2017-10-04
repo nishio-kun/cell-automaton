09bfc2e68adc5793c45376912b293d9a65be401e
yukinishio <yuki@localhost.localdomain>
2017-10-05 02:17:05 +0900

    zzz

---
+++ bomb.py
@@ -0,0 +1,9 @@
+#bomb
+
+def explode():
+    hypocenter = [random.randint(1,8), random.randint(1,8)]
+    field[hypocenter[0]][hypocenter[1]].newData = '#'
+    field[hypocenter[0]-1][hypocenter[1]].newData = '#'
+    field[hypocenter[0]+1][hypocenter[1]].newData = '#'
+    field[hypocenter[0]][hypocenter[1]-1].newData = '#'
+    field[hypocenter[0]][hypocenter[1]+1].newData = '#'
--- simulation2.py
+++ simulation2.py
@@ -38,41 +38,34 @@ class Cell:
     def pay_off(self):
         self.preData = self.newData
 
-#bomb
-def explode():
-    hypocenter = [random.randint(1,8), random.randint(1,8)]
-    field[hypocenter[0]][hypocenter[1]].newData = '#'
-    field[hypocenter[0]-1][hypocenter[1]].newData = '#'
-    field[hypocenter[0]+1][hypocenter[1]].newData = '#'
-    field[hypocenter[0]][hypocenter[1]-1].newData = '#'
-    field[hypocenter[0]][hypocenter[1]+1].newData = '#'
-    
 #make field
 field = [[] for i in range(10)]
-for i in range(10):
-    for j in range(10):
-        field[i].append(Cell(j, i))
+for y in range(10):
+    for x in range(10):
+        field[y].append(Cell(x,y))
 
 #initial value
 while True:
     start0 = [random.randint(0,9), random.randint(0,9)]
     start1 = [random.randint(0,9), random.randint(0,9)]
+    print(start0, start1)
 
     if start0 != start1:
-        field[start0[0]][start0[1]].preData = 0
-        field[start1[0]][start1[1]].preData = 1
+        field[start0[1]][start0[0]].preData = 0
+        field[start0[1]][start0[0]].newData = 0
+        field[start1[1]][start1[0]].preData = 1
+        field[start1[1]][start1[0]].newData = 1
         break
 
 #game
 while True:
-    command = input('q or Enter >> ')
+    command = input('Enter or q >> ')
     if command == 'q':
-        print('Good Bye')
         break
     else:
-        for i in range(10):
-            for j in range(10):
-                field[i][j].foster()
-                print(str(field[i][j].newData) + ' ', end='')
-                field[i][j].pay_off()
+        for y in range(10):
+            for x in range(10):
+                field[y][x].foster()
+                print(str(field[y][x].newData) + ' ', end='')
+                field[y][x].pay_off()
             print()
