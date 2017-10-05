b1d1c1b4934f8ef51f8662f1c1a4a746c7282eb6
yukinishio <yuki@localhost.localdomain>
2017-10-06 02:19:58 +0900

    Perfect ver

--- simulation.py
+++ simulation.py
@@ -1,85 +1,90 @@
-class Cell:
-    preData = '-'
-    newData = '-'
+import random
 
-    #def effect(self):
-
-    def pay_off(self):
-        self.preData = self.newData
-
-    def print_cell(self):
-        print(str(newData) + ' ', end='')
-
-def check(x):
-    if x in [0, 1]:
+def check_cell(num):
+    if num in [0, 1]:
         return True
     else:
         return False
 
-def process():
-    for i in range(10):
-        if i == 0:
-            if check(field[0][0].preData):
-                field[0][1].newData = field[0][0].preData
-                field[1][0].newData = field[0][0].preData
-            if check(field[0][9].preData):
-                field[0][8].newData = field[0][9].preData
-                field[1][9].newData = field[0][9].preData
-            for j in range(1, 9):
-                if check(field[0][j].preData):
-                    field[1][j].newData = field[0][j].preData
-                    field[0][j-1].newData = field[0][j].preData
-                    field[0][j+1].newData = field[0][j].preData
+class Cell:
+    x = 0
+    y = 0
+    preData = '-'
+    newData = '-'
 
-        elif i == 9:
-            if check(field[9][0].preData):
-                field[9][1].newData = field[9][0].preData
-                field[8][0].newData = field[9][0].preData
-            if check(field[9][9].preData):
-                field[9][8].newData = field[9][9].preData
-                field[8][9].newData = field[9][9].preData
-            for j in range(1, 9):
-                if check(field[9][j].preData):
-                    field[8][j].newData = field[9][j].preData
-                    field[9][j-1].newData = field[9][j].preData
-                    field[9][j+1].newData = field[9][j].preData
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-        else:
-            for j in range(10):
-                if j == 0:
-                    if check(field[i][0].preData):
-                        field[i-1][0].newData = field[i][0].preData
-                        field[i+1][0].newData = field[i][0].preData
-                        field[i][1].newData = field[i][0].preData
-                if j == 9:
-                    if check(field[i][9].preData):
-                        field[i-1][9].newData = field[i][9].preData
-                        field[i+1][9].newData = field[i][9].preData
-                        field[i][8].newData = field[i][9].preData
-                if j not in [0, 9] and check(field[i][j].preData):
-                    field[i-1][j].newData = field[i][j].preData
-                    field[i+1][j].newData = field[i][j].preData
-                    field[i][j-1].newData = field[i][j].preData
-                    field[i][j+1].newData = field[i][j].preData
+    def grow_north(self):
+        if check_cell(self.preData):
+            if y != 0:
+                try:
+                    field[self.y-1][self.x].newData = self.preData
+                except IndexError:
+                    pass
+
+    def grow_south(self):
+        if check_cell(self.preData):
+            try:
+                field[self.y+1][self.x].newData = self.preData
+            except IndexError:
+                pass
+
+    def grow_west(self):
+        if check_cell(self.preData):
+            if x != 0:
+                try:
+                    field[self.y][self.x-1].newData = self.preData
+                except IndexError:
+                    pass
+
+    def grow_east(self):
+        if check_cell(self.preData):
+            try:
+                field[self.y][self.x+1].newData = self.preData
+            except IndexError:
+                pass
+
+    def pay_off(self):
+        self.preData = self.newData
 
 #make field
-field = [[Cell() for i in range(10)] for j in range(10)]
+field = [['' for n in range(10)] for m in range(10)]
+for y in range(10):
+    for x in range(10):
+        field[y][x] = Cell(x,y)
 
-#initial value
-field[0][9].newData = 0
-field[9][0].newData = 1
-
-#main
 while True:
-    command = input('n or <Enter> >> ')
+    start0 = [random.randint(0,9), random.randint(0,9)]
+    start1 = [random.randint(0,9), random.randint(0,9)]
+
+    if start0 != start1:
+        field[start0[1]][start0[0]].preData = 0
+        field[start0[1]][start0[0]].newData = 0
+        field[start1[1]][start1[0]].preData = 1
+        field[start1[1]][start1[0]].newData = 1
+        break
+
+#game
+while True:
+    command = input('Enter or q >> ')
     if command == 'q':
-        print('Good Bye')
         break
     else:
-        process()
-
-        for i in range(10):
-            for j in range(10):
-                print(str(field[i][j].newData) + ' ', end='')
-                field[i][j].pay_off()
+        for y in range(10):
+            for x in range(10):
+                field[y][x].grow_north()
+                field[y][x].grow_south()
+                field[y][x].grow_west()
+                field[y][x].grow_east()
+        print()
+        for y in range(10):
+            print(' ', end='')
+            for x in range(10):
+                print(str(field[y][x].preData) + ' ', end='')
             print()
+        print()
+        for y in range(10):
+            for x in range(10):
+                field[y][x].pay_off()
