fb4058a316aea2f89a7fd3c4843ced90a8b61df2
yukinishio <yuki@localhost.localdomain>
2017-10-05 22:31:02 +0900

    better

--- simulation2.py
+++ simulation2.py
@@ -16,24 +16,37 @@ class Cell:
         self.x = x
         self.y = y
 
-    def foster(self):
+    def grow_north(self):
+        if check_cell(self.preData):
+            try:
+                field[self.y-1][self.x].newData = self.preData
+            except IndexError:
+                pass
+    '''
+
+    def grow_south(self):
+        if check_cell(self.preData):
+            try:
+                field[self.y+1][self.x].newData = self.preData
+            except IndexError:
+                pass
+    '''
+
+    def grow_west(self):
         if check_cell(self.preData):
             try:
                 field[self.y][self.x-1].newData = self.preData
             except IndexError:
                 pass
+    '''
+
+    def grow_east(self):
+        if check_cell(self.preData):
             try:
                 field[self.y][self.x+1].newData = self.preData
             except IndexError:
                 pass
-            try:
-                field[self.y-1][self.x].newData = self.preData
-            except IndexError:
-                pass
-            try:
-                field[self.y+1][self.x].newData = self.preData
-            except IndexError:
-                pass
+    '''
 
     def pay_off(self):
         self.preData = self.newData
@@ -45,17 +58,21 @@ for y in range(10):
         field[y].append(Cell(x,y))
 
 #initial value
+field[1][1].preData = 0
+field[8][8].preData = 1
+'''
 while True:
     start0 = [random.randint(0,9), random.randint(0,9)]
     start1 = [random.randint(0,9), random.randint(0,9)]
-    print(start0, start1)
 
     if start0 != start1:
+        print(start0, start1)
         field[start0[1]][start0[0]].preData = 0
-        field[start0[1]][start0[0]].newData = 0
+        #field[start0[1]][start0[0]].newData = 0
         field[start1[1]][start1[0]].preData = 1
-        field[start1[1]][start1[0]].newData = 1
+        #field[start1[1]][start1[0]].newData = 1
         break
+'''
 
 #game
 while True:
@@ -63,9 +80,15 @@ while True:
     if command == 'q':
         break
     else:
+        print()
         for y in range(10):
+            print(' ', end='')
             for x in range(10):
-                field[y][x].foster()
+                field[y][x].grow_north()
+                #field[y][x].grow_south()
+                field[y][x].grow_west()
+                #field[y][x].grow_east()
                 print(str(field[y][x].newData) + ' ', end='')
                 field[y][x].pay_off()
             print()
+        print()
