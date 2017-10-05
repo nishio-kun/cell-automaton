92eccf1311ab13da84c916428a0642209873fd2b
yukinishio <yuki@localhost.localdomain>
2017-10-06 02:14:29 +0900

    Completegit add simulation2.py

--- simulation2.py
+++ simulation2.py
@@ -18,11 +18,11 @@ class Cell:
 
     def grow_north(self):
         if check_cell(self.preData):
-            try:
-                field[self.y-1][self.x].newData = self.preData
-            except IndexError:
-                pass
-    '''
+            if y != 0:
+                try:
+                    field[self.y-1][self.x].newData = self.preData
+                except IndexError:
+                    pass
 
     def grow_south(self):
         if check_cell(self.preData):
@@ -30,15 +30,14 @@ class Cell:
                 field[self.y+1][self.x].newData = self.preData
             except IndexError:
                 pass
-    '''
 
     def grow_west(self):
         if check_cell(self.preData):
-            try:
-                field[self.y][self.x-1].newData = self.preData
-            except IndexError:
-                pass
-    '''
+            if x != 0:
+                try:
+                    field[self.y][self.x-1].newData = self.preData
+                except IndexError:
+                    pass
 
     def grow_east(self):
         if check_cell(self.preData):
@@ -46,7 +45,6 @@ class Cell:
                 field[self.y][self.x+1].newData = self.preData
             except IndexError:
                 pass
-    '''
 
     def pay_off(self):
         self.preData = self.newData
@@ -57,24 +55,16 @@ for y in range(10):
     for x in range(10):
         field[y][x] = Cell(x,y)
 
-#initial value
-field[1][1].preData = 0
-field[1][1].newData = 0
-field[8][8].preData = 1
-field[8][8].newData = 1
-'''
 while True:
     start0 = [random.randint(0,9), random.randint(0,9)]
     start1 = [random.randint(0,9), random.randint(0,9)]
 
     if start0 != start1:
-        print(start0, start1)
         field[start0[1]][start0[0]].preData = 0
-        #field[start0[1]][start0[0]].newData = 0
+        field[start0[1]][start0[0]].newData = 0
         field[start1[1]][start1[0]].preData = 1
-        #field[start1[1]][start1[0]].newData = 1
+        field[start1[1]][start1[0]].newData = 1
         break
-'''
 
 #game
 while True:
@@ -82,15 +72,19 @@ while True:
     if command == 'q':
         break
     else:
+        for y in range(10):
+            for x in range(10):
+                field[y][x].grow_north()
+                field[y][x].grow_south()
+                field[y][x].grow_west()
+                field[y][x].grow_east()
         print()
         for y in range(10):
             print(' ', end='')
             for x in range(10):
-                field[y][x].grow_north()
-                #field[y][x].grow_south()
-                field[y][x].grow_west()
-                #field[y][x].grow_east()
                 print(str(field[y][x].preData) + ' ', end='')
-                field[y][x].pay_off()
             print()
         print()
+        for y in range(10):
+            for x in range(10):
+                field[y][x].pay_off()
